0908979f7ab4986f7240cbc38940ac50ff23b224
umwalikara <umwaliclara@gmail.com>
2019-09-06 11:44:38 +0200

    adding user class

---
+++ user.py
@@ -0,0 +1,8 @@
+class User:   
+    user_list = [] 
+    def __init__(self,username,password):
+        self.username=username
+        self.password=password
+        
+    def save_user(self):
+        User.user_list.append(self)
