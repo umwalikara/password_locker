5db74feffe26a7517adfb791b219768f6c635b85
umwalikara <umwaliclara@gmail.com>
2019-09-06 17:08:35 +0200

    saving a user and credential in run.py

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 from user import User
-def create_user(username,password):
+def create_user(username,password): #creating user
     new_user=User(username,password)
     return new_user
+def save_user(user): #function save user
+    user.save_user() 
+    
+
 
 from credentials import Credential
-def create_credential(accountname,username,password):
+def create_credential(accountname,username,password):#creating credential
     new_credential=Credential(accountname,username,password)
     return new_credential
+
+def save_credential(credential): #function to save crede
+    credential.save_credential
