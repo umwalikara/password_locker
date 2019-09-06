bc3be8c1544043e499c08b3f53650b659cdbc326
umwalikara <umwaliclara@gmail.com>
2019-09-06 18:11:07 +0200

    adding function to check in user and credential class run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3.6
 from user import User
-def create_user(username,password): #creating user
-    new_user=User(username,password)
+def create_user(username, password):
+    new_user = User(username,password)
     return new_user
 
 def save_user(user): #function save user
@@ -11,6 +12,10 @@ def del_user(user): #function to delete user
 
 def find_user(username): #function that  finding user
     return User.find_by_username(username)
+
+def check_existing_user(username):#Function that check if a contact exists with that user
+
+    return User.user_exist(username)
 
 def display_user(): #function that return all the save user
     return User.display_user()
@@ -31,8 +36,8 @@ def del_credential(credential): #function to delete crede
 def find_credential(accountname): #function that finding credential
     return Credential.find_by_accountname(accountname)
 
-# def check_existing_credential(accountname)
-
+def check_existing_credential(accountname): #Function that check if a contact exists with that accountname
+    return Credential.credential_exist(accountname)
 
 def display_credential(): #Function that returns all the saved contacts
     return Credential.display_credential()
