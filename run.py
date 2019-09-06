bd1b7eba9c49af7c6b25a13200c1e5133c74c2d5
umwalikara <umwaliclara@gmail.com>
2019-09-06 17:19:27 +0200

    deleting a user and credential in run.py

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ from user import User
 def create_user(username,password): #creating user
     new_user=User(username,password)
     return new_user
+
 def save_user(user): #function save user
     user.save_user() 
-    
+
+def del_user(user): #function to delete user
+    user.delete_user
+
 
 
 from credentials import Credential
@@ -14,3 +18,6 @@ def create_credential(accountname,username,password):#creating credential
 
 def save_credential(credential): #function to save crede
     credential.save_credential
+
+def del_credential(credential): #function to delete crede
+    credential.delete_credential
