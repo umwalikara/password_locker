564ecac605789ed338d5595e8c487ad4c4021693
umwalikara <umwaliclara@gmail.com>
2019-09-06 17:43:31 +0200

    dispalying a user and credential in run.py

--- run.py
+++ run.py
@@ -9,8 +9,11 @@ def save_user(user): #function save user
 def del_user(user): #function to delete user
     user.delete_user
 
-def find_user(username): #finding user
+def find_user(username): #function that  finding user
     return User.find_by_username(username)
+
+def display_user(): #function that return all the save user
+    return User.display_user()
 
 
 
@@ -25,5 +28,11 @@ def save_credential(credential): #function to save crede
 def del_credential(credential): #function to delete crede
     credential.delete_credential
 
-def find_credential(accountname): #finding credential
+def find_credential(accountname): #function that finding credential
     return Credential.find_by_accountname(accountname)
+
+# def check_existing_credential(accountname)
+
+
+def display_credential(): #Function that returns all the saved contacts
+    return Credential.display_credential()
